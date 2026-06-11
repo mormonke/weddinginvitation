e8e39672619e1eec14887497727b65b17d12b988
monk <danyamiki04@gmail.com>
2026-06-11 21:30:08 +0600

    back1

--- vite.config.ts
+++ vite.config.ts
@@ -17,7 +17,7 @@ function figmaAssetResolver() {
 }
 
 export default defineConfig({
-  base: '/weddinginvitation',
+  base: './weddinginvitation',
   plugins: [
     figmaAssetResolver(),
     // The React and Tailwind plugins are both required for Make, even if
